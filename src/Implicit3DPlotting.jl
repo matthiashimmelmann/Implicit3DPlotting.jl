50e77b8b2863c7ebf4bbf8f39c6bfab700760454
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2021-04-26 18:27:06 +0200

    update

--- src/Implicit3DPlotting.jl
+++ src/Implicit3DPlotting.jl
@@ -1,14 +1,20 @@
 module Implicit3DPlotting
 
-export plot_implicit_surface,
-       plot_implicit_surface!,
-       plot_implicit_curve,
-       plot_implicit_curve!
 
-import GLMakie: xlims!, ylims!, zlims!, wireframe!, linesegments!, mesh!, Scene, cam3d!, Point3f0
+import GLMakie: xlims!, ylims!, zlims!, wireframe!, linesegments!, mesh!, Scene, cam3d!, Point3f0, scatter!
 import Meshing: MarchingCubes, MarchingTetrahedra
 import GLMakie.GeometryBasics: Mesh, Rect, Vec, decompose, TriangleFace, Point
 import Polyhedra: vrep, intersect, polyhedron
+
+export plot_implicit_surface,
+       plot_implicit_surface!,
+       plot_implicit_curve,
+       plot_implicit_curve!,
+       makieScatter!
+
+function makieScatter!(kwargs...)
+    scatter!(kwargs...)
+end
 
 """
 Adds an implicitly defined surface to the scene.
