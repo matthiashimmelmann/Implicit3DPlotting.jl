8c5440f7e9cd7c221a2d3cc3a923d81d0ec88ab5
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2023-08-02 17:19:15 +0200

    Update Implicit3DPlotting.jl

--- src/Implicit3DPlotting.jl
+++ src/Implicit3DPlotting.jl
@@ -88,7 +88,7 @@ function plot_implicit_surface!(
         end
     end
 
-    scale!(scene, scaling[1], scaling[2], scaling[3])
+    #scale!(scene, scaling[1], scaling[2], scaling[3])
     return(scene)
 end
 
@@ -112,7 +112,12 @@ function plot_implicit_surface(
     else
         GLMakiePlottingLibrary.activate!()
         #GLMakiePlottingLibrary.AbstractPlotting.inline!(in_line)
-        scene = GLMakiePlottingLibrary.Scene(resolution=resolution, scale_plot=scale_plot, camera=cam3d!, show_axis=show_axis)
+        figure = GLMakiePlottingLibrary.Figure(resolution=resolution, scale_plot=scale_plot)
+        scene = GLMakiePlottingLibrary.Axis3(figure[1,1])
+        if !show_axis
+            GLMakiePlottingLibrary.hidespines!(scene)
+            GLMakiePlottingLibrary.hidedecorations!(scene)
+        end
     end
     plot_implicit_surface!(scene, f; WGLMode=WGLMode, transparency=transparency, kwargs...)
     return(scene)
@@ -196,7 +201,7 @@ function plot_implicit_curve!(
         end
     end
 
-    scale!(scene, scaling[1], scaling[2], scaling[3])
+    #scale!(scene, scaling[1], scaling[2], scaling[3])
     return(scene)
 end
 
@@ -217,13 +222,18 @@ function plot_implicit_curve(
         WGLMakiePlottingLibrary.activate!()
         #WGLMakiePlottingLibrary.AbstractPlotting.inline!(in_line)
         scene = WGLMakiePlottingLibrary.Scene(resolution=resolution, scale_plot=scale_plot, camera=WGLMakiePlottingLibrary.cam3d!, show_axis=show_axis)
-        plot_implicit_curve!(scene, f, g; WGLMode=WGLMode, kwargs...)
     else
         GLMakiePlottingLibrary.activate!()
         #GLMakiePlottingLibrary.AbstractPlotting.inline!(in_line)
-        scene = GLMakiePlottingLibrary.Scene(resolution=resolution, scale_plot=scale_plot, camera=cam3d!, show_axis=show_axis)
-        plot_implicit_curve!(scene, f, g; WGLMode=WGLMode, kwargs...)
+        figure = GLMakiePlottingLibrary.Figure(resolution=resolution, scale_plot=scale_plot)
+        scene = GLMakiePlottingLibrary.Axis3(figure[1,1])
+        if !show_axis
+            GLMakiePlottingLibrary.hidespines!(scene)
+            GLMakiePlottingLibrary.hidedecorations!(scene)
+        end
     end
+    plot_implicit_curve!(scene, f, g; WGLMode=WGLMode, kwargs...)
+
     return(scene)
 end
 
