be8c2f51bb0a534cc21aaf3f1b9e6a45c47f6ce7
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-04-03 05:26:20 +0200

    updated to version 0.5

--- src/Implicit3DPlotting.jl
+++ src/Implicit3DPlotting.jl
@@ -17,6 +17,8 @@ import WGLMakie as WGLMakiePlottingLibrary
 import Meshing: MarchingCubes, MarchingTetrahedra
 import GLMakie.GeometryBasics: Mesh, Rect, Vec, decompose, TriangleFace, Point, SimpleFaceView
 import Polyhedra: vrep, intersect, polyhedron
+import LinearAlgebra: pinv, norm, nullspace
+import HomotopyContinuation: evaluate, differentiate, @var, Expression
 
 export plot_implicit_surface,
        plot_implicit_surface!,
@@ -109,11 +111,12 @@ Plots an implicitly defined surface.
 function plot_implicit_surface(
     f::Function;
     show_axis = true,
-    resolution=(800,800),
+    resolution=(820,800),
     aspect=(1.,1.,1.),
     WGLMode=false,
     in_line=false,
-    transparency=true, 
+    transparency=true,
+    fontsize=17,
     kwargs...
 )
     if WGLMode
@@ -123,7 +126,7 @@ function plot_implicit_surface(
         GLMakiePlottingLibrary.activate!()
         #GLMakiePlottingLibrary.AbstractPlotting.inline!(in_line)
         global fig = GLMakiePlottingLibrary.Figure(size=resolution)
-        ax = GLMakiePlottingLibrary.Axis3(fig[1,1], aspect = aspect)
+        ax = GLMakiePlottingLibrary.Axis3(fig[1,1], aspect = aspect, xlabelsize=fontsize, ylabelsize=fontsize, zlabelsize=fontsize)
         if !show_axis
             GLMakiePlottingLibrary.hidespines!(ax)
             GLMakiePlottingLibrary.hidedecorations!(ax)
@@ -150,12 +153,14 @@ function plot_implicit_curve!(
     ylims::Tuple{Union{Float64,Int},Union{Float64,Int}} = (y_min, y_max),
     zlims::Tuple{Union{Float64,Int},Union{Float64,Int}} = (z_min, z_max),
     color::Symbol = :steelblue,
-    samples::Tuple{Int,Int,Int}=(30,30,30),
-    linewidth::Union{Float64,Int}=1.5,
+    samples::Tuple{Int,Int,Int}=(10,10,10),
+    linewidth::Union{Float64,Int}=2.25,
     MarchingModeIsCubes::Bool = true,
     WGLMode::Bool = false,
+    step_size=0.025,
     transparency::Bool = true,
     cutoffmap=nothing,
+    miniter=5,
     kwargs...
 )
     if WGLMode
@@ -170,18 +175,75 @@ function plot_implicit_curve!(
     catch
         throw("One of the specified function does not take 3 arguments.")
     end
-    f_implicit_mesh = Mesh(f, Rect(Vec(xlims[1], ylims[1],zlims[1]),
-                            Vec(xlims[2]-xlims[1], ylims[2]-ylims[1],zlims[2]-zlims[1])), samples=samples, MarchingModeIsCubes ? MarchingCubes() : MarchingTetrahedra())
-    g_implicit_mesh = Mesh(g, Rect(Vec(xlims[1], ylims[1],zlims[1]),
-                            Vec(xlims[2]-xlims[1], ylims[2]-ylims[1],zlims[2]-zlims[1])), samples=samples, MarchingModeIsCubes ? MarchingCubes() : MarchingTetrahedra())
 
     lines=[]
-    for triangle_f in f_implicit_mesh, triangle_g in g_implicit_mesh
-        if(min(sum((triangle_f[1]-triangle_g[1]).^2), sum((triangle_f[2]-triangle_g[2]).^2), sum((triangle_f[3]-triangle_g[3]).^2))
-                < max(sum((triangle_f[1]-triangle_f[2]).^2), sum((triangle_f[2]-triangle_f[3]).^2), sum((triangle_g[3]-triangle_g[1]).^2), sum((triangle_g[1]-triangle_g[2]).^2), sum((triangle_g[2]-triangle_g[3]).^2), sum((triangle_g[3]-triangle_g[1]).^2)))
-            intersection=intersectTwoTriangles(triangle_f, triangle_g)
-            if(length(intersection)>=2) && (cutoffmap==nothing || cutoffmap(intersection[1]) && cutoffmap(intersection[2]))
-                push!(lines, [Point3f0(intersection[1]), Point3f0(intersection[2])])
+
+    try
+        @var x[1:3]
+        poly_f = Expression(f(x))
+        poly_g = Expression(g(x))
+        point_samples = []
+        helper_lines, lines = [], []
+        poly_sys = [poly_f, poly_g]
+        jac = differentiate(poly_sys, x)
+        for i in 1:Int(round(sum(samples)/3))
+            lin_space = (rand(Float64,3) .- 0.5)'*x
+            lin_jac = differentiate(vcat(poly_sys, lin_space), x)
+            for _ in 1:2
+                start_point = 0.2*(rand(Float64,3) .- 0.5)
+                try
+                    q = newtoncorrect(vcat(poly_sys, lin_space), x, lin_jac, start_point)
+                    if !any(t->isapprox(norm(t-q),0; atol=1e-10), point_samples)
+                        push!(point_samples, q)
+                    end
+                catch
+                    continue
+                end
+            end
+        end
+
+        for point_sample in point_samples
+            one_line = [Point3f0(point_sample)]
+            q = Base.copy(point_sample)
+            prev_flex = nullspace(evaluate(jac, x=>point_sample))[:,1]
+            i = 1
+            while true
+                q, prev_flex = euler_step(jac, x, step_size, prev_flex, q)
+                q = newtoncorrect(poly_sys, x, jac, q)
+                push!(one_line, Point3f0(q))
+                i=i+1
+                if i<miniter
+                    continue
+                end
+                if (cutoffmap!=nothing && !cutoffmap(q)) || (q[1]<xlims[1] || q[1]>xlims[2] || q[2]<ylims[1] || q[2]>ylims[2] || q[3]<zlims[1] || q[3]>zlims[2] ) || isapprox(norm(q-point_sample),0; atol=step_size)
+                    break
+                end
+            end
+            push!(helper_lines, one_line)
+        end
+
+        for line in helper_lines
+            for i in 1:length(line)-1
+                if !any(tl->isapprox(tl[1], line[i], atol=step_size/2) && isapprox(tl[2], line[i+1], atol=step_size/2), lines) && !any(tl->isapprox(tl[2], line[i], atol=step_size/2) && isapprox(tl[1], line[i+1], atol=step_size/2), lines)
+                    push!(lines, [line[i], line[i+1]])
+                end
+            end
+        end
+
+    catch e
+        @warn "The given functions f and g could not be cast to polynomials. Error code: $(e)."
+        f_implicit_mesh = Mesh(f, Rect(Vec(xlims[1], ylims[1],zlims[1]),
+                                Vec(xlims[2]-xlims[1], ylims[2]-ylims[1],zlims[2]-zlims[1])), samples=samples, MarchingModeIsCubes ? MarchingCubes() : MarchingTetrahedra())
+        g_implicit_mesh = Mesh(g, Rect(Vec(xlims[1], ylims[1],zlims[1]),
+                                Vec(xlims[2]-xlims[1], ylims[2]-ylims[1],zlims[2]-zlims[1])), samples=samples, MarchingModeIsCubes ? MarchingCubes() : MarchingTetrahedra())
+
+        for triangle_f in f_implicit_mesh, triangle_g in g_implicit_mesh
+            if(min(sum((triangle_f[1]-triangle_g[1]).^2), sum((triangle_f[2]-triangle_g[2]).^2), sum((triangle_f[3]-triangle_g[3]).^2))
+                    < max(sum((triangle_f[1]-triangle_f[2]).^2), sum((triangle_f[2]-triangle_f[3]).^2), sum((triangle_g[3]-triangle_g[1]).^2), sum((triangle_g[1]-triangle_g[2]).^2), sum((triangle_g[2]-triangle_g[3]).^2), sum((triangle_g[3]-triangle_g[1]).^2)))
+                intersection=intersectTwoTriangles(triangle_f, triangle_g)
+                if(length(intersection)>=2) && (cutoffmap==nothing || cutoffmap(intersection[1]) && cutoffmap(intersection[2]))
+                    push!(lines, [Point3f0(intersection[1]), Point3f0(intersection[2])])
+                end
             end
         end
     end
@@ -192,18 +254,18 @@ function plot_implicit_curve!(
         # 3*linewidth, as the lines seem to be drawn way thinner than in GLMakie
         try
             foreach(line->WGLMakiePlottingLibrary.linesegments!(ax, line; transparency=transparency, color=color, linewidth=3*linewidth, kwargs...), lines)
-            WGLMakiePlottingLibrary.xlims!(ax, (xlims[1],xlims[2]))
-            WGLMakiePlottingLibrary.ylims!(ax, (ylims[1],ylims[2]))
-            WGLMakiePlottingLibrary.zlims!(ax, (zlims[1],zlims[2]))
+            WGLMakiePlottingLibrary.xlims!(ax, xlims[1],xlims[2])
+            WGLMakiePlottingLibrary.ylims!(ax, ylims[1],ylims[2])
+            WGLMakiePlottingLibrary.zlims!(ax, zlims[1],zlims[2])
         catch e
             println("No curve in WebGL-Mode detected! Check for the relative generality of the implicit surfaces! Error code: $(e)")
         end
     else
         try
             foreach(line->linesegments!(ax, line; color=color, transparency=transparency, linewidth=linewidth, kwargs...), lines)
-            xlims!(ax, (xlims[1],xlims[2]))
-            ylims!(ax, (ylims[1],ylims[2]))
-            zlims!(ax, (zlims[1],zlims[2]))
+            xlims!(xlims[1],xlims[2])
+            ylims!(ylims[1],ylims[2])
+            zlims!(zlims[1],zlims[2])
         catch e
             println("No curve in OpenGL-Mode detected! Check for the relative generality of the implicit surfaces! Error code: $(e)")
         end
@@ -216,8 +278,9 @@ Plots a space curve, implicitly defined by two equations.
 function plot_implicit_curve(
     f::Function,
     g::Function;
-    resolution=(800,800),
+    resolution=(820,800),
     aspect=(1.,1.,1.),
+    fontsize=17,
     show_axis=true,
     in_line=false,
     WGLMode=false,
@@ -231,7 +294,7 @@ function plot_implicit_curve(
         GLMakiePlottingLibrary.activate!()
         #GLMakiePlottingLibrary.AbstractPlotting.inline!(in_line)
         global fig = GLMakiePlottingLibrary.Figure(size=resolution)
-        ax = GLMakiePlottingLibrary.Axis3(fig[1,1], aspect = aspect)
+        ax = GLMakiePlottingLibrary.Axis3(fig[1,1], aspect = aspect, xlabelsize=fontsize, ylabelsize=fontsize, zlabelsize=fontsize)
         if !show_axis
             GLMakiePlottingLibrary.hidespines!(ax)
             GLMakiePlottingLibrary.hidedecorations!(ax)
@@ -252,5 +315,45 @@ function intersectTwoTriangles(triangle1, triangle2)
     intersection = intersect(polyhedron1, polyhedron2)
     return(vrep(intersection).points.points)
 end
+
+"""
+Implementation of Newton's method
+"""
+function newtoncorrect(equations, variables, jac, point; tol = 1e-14)
+    _time = Base.time()
+	q = Base.copy(point)
+	global damping = 0.15
+	while(norm(evaluate(equations, variables=>q)) > tol)
+        if Base.time() - _time > 5
+            throw(error("Error_time"))
+        end
+		J = evaluate.(jac, variables=>q)
+		qnew = q - damping*pinv(J)*evaluate(equations, variables=>q)
+		if norm(evaluate(equations, variables=>qnew)) < norm(evaluate(equations, variables=>q))
+			global damping = damping*1.1
+		else
+			global damping = damping/2
+		end
+        if damping < 1e-15
+            throw(error("Error_scale"))
+        end
+        q = qnew
+		if damping > 1
+			global damping = 1
+		end
+	end
+	return q
+end
+
+
+function euler_step(jacobian, variables, step_size::Float64, prev_flex::Vector{Float64}, point::Union{Vector{Int},Vector{Float64}})
+    J = evaluate(jacobian, variables=>point)
+    flex_space = nullspace(J)[:,1]
+    flex_coefficients = pinv(flex_space) * prev_flex
+    predicted_inf_flex = sum(flex_space[:,i] .* flex_coefficients[i] for i in 1:length(flex_coefficients))
+    predicted_inf_flex = predicted_inf_flex ./ norm(predicted_inf_flex)
+    return point+step_size*predicted_inf_flex, predicted_inf_flex
+end
+
 
 end
